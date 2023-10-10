ef392fe2e298f551ad0532377607c2ee3b5ec8d6
minthing <121234836+milliekang@users.noreply.github.com>
2023-10-10 15:09:33 +0900

    install firebase

---
+++ src/routes/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC1o4iUN_fLF2C4VWqsx2RwtDhmgMUNmQc",
+  authDomain: "twitter-clone-d0256.firebaseapp.com",
+  projectId: "twitter-clone-d0256",
+  storageBucket: "twitter-clone-d0256.appspot.com",
+  messagingSenderId: "846963963671",
+  appId: "1:846963963671:web:3bb9d8f630bb7591b7e952"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
