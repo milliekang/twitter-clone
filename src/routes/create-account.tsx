81d7846d8e74e22955637cbb76dd99e2939aefc3
minthing <121234836+milliekang@users.noreply.github.com>
2023-10-13 05:46:48 +0900

    login form

--- src/routes/create-account.tsx
+++ src/routes/create-account.tsx
@@ -2,12 +2,44 @@ import { useState } from "react";
 import styled from "styled-components"
 
 
-const Wrapper = styled.div``;
+const Wrapper = styled.div`
+height:100%;
+display: flex;
+flex-direction: column;
+align-items: center;
+width: 420px;
+padding:50px 0;
+`;
 
-const Form = styled.form``;
+const Form = styled.form`
+margin-top: 50px;
+display: flex;
+flex-direction: column;
+gap: 10px;
+width: 100%;
+`;
 
-const Input = styled.input``;
+const Input = styled.input`
+padding: 10px 20px;
+border-radius: 50px;
+border: none;
+width: 100%;
+font-size:16px;
+&[type="submit"]{
+  cursor: pointer;
+  &:hover{
+    opacity: 0.5;
+  }
+}
+`;
 
+const Title = styled.h1`
+font-size: 52px;
+`
+
+const Error = styled.p`
+color:red;
+`
 
 export default function CreateAccount(){
 
@@ -15,6 +47,7 @@ export default function CreateAccount(){
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [error, setError] = useState("");
 
   const onChange = (e:React.ChangeEvent<HTMLInputElement>) => {
     const {target : {name, value}} = e;
@@ -28,12 +61,20 @@ export default function CreateAccount(){
     }
   }
 
+  const onSubmit = (e:React.FormEvent<HTMLFormElement>) =>{
+    e.preventDefault();
+    console.log(name, email, password);
+    try{}catch(e){}finally{}
+  }
+
   return <Wrapper>
-    <Form>
+    <Title>Login To Twitter</Title> 
+    <Form onSubmit={onSubmit}>
       <Input onChange={onChange} name="name" placeholder="name" value={name} type="text" required />
       <Input onChange={onChange} name="email" placeholder="email" value={email} type="email" required/>
       <Input onChange={onChange} name="password" placeholder="password" value={password} type="password" required/>
-      <Input onChange={onChange} type="submit" value="create account" />
+      <Input onChange={onChange} type="submit" value={isLoading? "Loading" : "Create Account"} />
     </Form>
+    {error !== "" ? <Error>{error}</Error>:''}
   </Wrapper>
 }
