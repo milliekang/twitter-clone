cce74e3c935206085f87e20ba5ce1c8e66a71ac1
minthing <121234836+milliekang@users.noreply.github.com>
2023-10-15 10:36:46 +0900

    logout method & protected router

---
+++ src/components/protected-route.tsx
@@ -0,0 +1,12 @@
+import { Navigate } from "react-router-dom";
+import { auth } from "../routes/firebase"
+
+export default function ProtectedRoute({children} : {children : React.ReactNode}){
+
+  const user = auth.currentUser;
+  if(user === null){
+    return <Navigate to="/login" />
+  }
+  console.log(user)
+  return children
+}
